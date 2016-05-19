cb279c8cc42f5d27f1c4b78eadf9fcdcd0100f21
YAMAMOTO Takashi <yamamoto@midokura.com>
2016-05-19 17:07:30 +0900

    router-interface-fip extension
    Related-Bug: #1556884
    Related-Bug: #1566191
    Change-Id: I6fb249eb85eaf475acdb00e4d48f1aa531d95d12

--- midonet/neutron/services/l3/l3_midonet.py
+++ midonet/neutron/services/l3/l3_midonet.py
@@ -13,14 +13,22 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import netaddr
+from sqlalchemy import orm
+
 from midonet.neutron._i18n import _LE, _LW
 from midonet.neutron.client import base as c_base
 from midonet.neutron.common import config  # noqa
 from midonet.neutron.common import constants as m_const
 from midonet.neutron.db import l3_db_midonet
 from midonet.neutron import extensions
+from midonet.neutron.extensions import routerinterfacefip
 
 from neutron.api import extensions as neutron_extensions
+from neutron.callbacks import events
+from neutron.callbacks import exceptions
+from neutron.callbacks import registry
+from neutron.callbacks import resources
 from neutron.common import constants as n_const
 from neutron.common import exceptions as n_exc
 from neutron.db import common_db_mixin
@@ -28,6 +36,7 @@ from neutron.db import extraroute_db
 from neutron.db import l3_db
 # Import l3_dvr_db to get the config options required for FWaaS
 from neutron.db import l3_dvr_db  # noqa
+from neutron.db import models_v2
 from neutron.extensions import l3
 from neutron.extensions import multiprovidernet as mpnet
 from neutron.extensions import providernet as pnet
@@ -48,11 +57,13 @@ class MidonetL3ServicePlugin(common_db_mixin.CommonDbMixin,
     Implements L3 Router service plugin for Midonet.
     """
 
-    supported_extension_aliases = ["router", "extraroute", "ext-gw-mode"]
+    supported_extension_aliases = ["router", "extraroute", "ext-gw-mode",
+                                   "router-interface-fip"]
 
     def __init__(self):
         super(MidonetL3ServicePlugin, self).__init__()
         l3_db.subscribe()
+        self.__subscribe()
 
         # Instantiate MidoNet API client
         self.client = c_base.load_client(cfg.CONF.MIDONET)
@@ -255,3 +266,120 @@ class MidonetL3ServicePlugin(common_db_mixin.CommonDbMixin,
                     LOG.exception(_LE("Failed to update floating ip "
                                       "status %s"), id)
         return fip
+
+    def get_router_for_floatingip(self, context, internal_port,
+            internal_subnet, external_network_id):
+        # REVISIT(yamamoto): These direct manipulation of core-plugin db
+        # resources is not ideal.
+        gw_port = orm.aliased(models_v2.Port, name="gw_port")
+        routerport_qry = context.session.query(
+            l3_db.RouterPort.router_id,
+            models_v2.IPAllocation.ip_address
+        ).join(
+            models_v2.Port, models_v2.IPAllocation
+        ).filter(
+            models_v2.Port.network_id == internal_port['network_id'],
+            l3_db.RouterPort.port_type.in_(
+                n_const.ROUTER_INTERFACE_OWNERS
+            ),
+            models_v2.IPAllocation.subnet_id == internal_subnet['id']
+        ).join(
+            gw_port, gw_port.device_id == l3_db.RouterPort.router_id
+        ).filter(
+            gw_port.network_id == external_network_id,
+        ).distinct()
+
+        first_router_id = None
+        for router_id, interface_ip in routerport_qry:
+            if interface_ip == internal_subnet['gateway_ip']:
+                return router_id
+            if not first_router_id:
+                first_router_id = router_id
+        if first_router_id:
+            return first_router_id
+
+        raise l3.ExternalGatewayForFloatingIPNotFound(
+            subnet_id=internal_subnet['id'],
+            external_network_id=external_network_id,
+            port_id=internal_port['id'])
+
+    def _subnet_has_fip(self, context, router_id, subnet_id):
+        # Return True if the subnet has one of floating IPs for the router
+        subnet = self._core_plugin.get_subnet(context, subnet_id)
+        subnet_cidr = netaddr.IPNetwork(subnet['cidr'])
+        fip_qry = context.session.query(l3_db.FloatingIP)
+        fip_qry = fip_qry.filter_by(router_id=router_id)
+        for fip_db in fip_qry:
+            if netaddr.IPAddress(fip_db['floating_ip_address']) in subnet_cidr:
+                return True
+        return False
+
+    # REVISIT(yamamoto): This method is a copy of the base class method,
+    # with modified RouterExternalGatewayInUseByFloatingIp validation.
+    def _delete_current_gw_port(self, context, router_id, router,
+                                new_network_id):
+        """Delete gw port if attached to an old network."""
+        port_requires_deletion = (
+            router.gw_port and router.gw_port['network_id'] != new_network_id)
+        if not port_requires_deletion:
+            return
+        admin_ctx = context.elevated()
+        old_network_id = router.gw_port['network_id']
+
+        for ip in router.gw_port['fixed_ips']:
+            if self._subnet_has_fip(admin_ctx, router_id, ip['subnet_id']):
+                raise l3.RouterExternalGatewayInUseByFloatingIp(
+                    router_id=router_id, net_id=router.gw_port['network_id'])
+        gw_ips = [x['ip_address'] for x in router.gw_port.fixed_ips]
+        with context.session.begin(subtransactions=True):
+            gw_port = router.gw_port
+            router.gw_port = None
+            context.session.add(router)
+            context.session.expire(gw_port)
+            self._check_router_gw_port_in_use(context, router_id)
+        self._core_plugin.delete_port(
+            admin_ctx, gw_port['id'], l3_port_check=False)
+        registry.notify(resources.ROUTER_GATEWAY,
+                        events.AFTER_DELETE, self,
+                        router_id=router_id,
+                        network_id=old_network_id,
+                        gateway_ips=gw_ips)
+
+    # REVISIT(yamamoto): This method is a copy of the base class method,
+    # with 'router_id' notification argument added.
+    def _confirm_router_interface_not_in_use(self, context, router_id,
+                                             subnet_id):
+        subnet = self._core_plugin.get_subnet(context, subnet_id)
+        subnet_cidr = netaddr.IPNetwork(subnet['cidr'])
+        fip_qry = context.session.query(l3_db.FloatingIP)
+        try:
+            kwargs = {'context': context, 'subnet_id': subnet_id,
+                      'router_id': router_id}
+            registry.notify(
+                resources.ROUTER_INTERFACE,
+                events.BEFORE_DELETE, self, **kwargs)
+        except exceptions.CallbackFailure as e:
+            with excutils.save_and_reraise_exception():
+                # NOTE(armax): preserve old check's behavior
+                if len(e.errors) == 1:
+                    raise e.errors[0].error
+                raise l3.RouterInUse(router_id=router_id, reason=e)
+        for fip_db in fip_qry.filter_by(router_id=router_id):
+            if netaddr.IPAddress(fip_db['fixed_ip_address']) in subnet_cidr:
+                raise l3.RouterInterfaceInUseByFloatingIP(
+                    router_id=router_id, subnet_id=subnet_id)
+
+    def _check_router_interface_used_as_gw_for_fip(self, resource,
+                                                   event, trigger, **kwargs):
+        context = kwargs['context']
+        router_id = kwargs['router_id']
+        subnet_id = kwargs['subnet_id']
+        if self._subnet_has_fip(context, router_id, subnet_id):
+            raise routerinterfacefip.RouterInterfaceInUseAsGatewayByFloatingIP(
+                router_id=router_id, subnet_id=subnet_id)
+
+    def __subscribe(self):
+        registry.subscribe(
+            self._check_router_interface_used_as_gw_for_fip,
+            resources.ROUTER_INTERFACE,
+            events.BEFORE_DELETE)
