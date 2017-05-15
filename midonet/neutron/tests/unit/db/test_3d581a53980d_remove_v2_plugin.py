0145adba6aeed2f1542d9963688cc0158cf78ba1
YAMAMOTO Takashi <yamamoto@midokura.com>
2017-05-16 08:09:18 +0900

    x
    Change-Id: Icae39c70cc3438fc443449ecb474ad6f48e0df2a

--- midonet/neutron/tests/unit/db/test_3d581a53980d_remove_v2_plugin.py
+++ midonet/neutron/tests/unit/db/test_3d581a53980d_remove_v2_plugin.py
@@ -20,7 +20,7 @@ from neutron.tests.functional.db import test_migrations
 
 
 class RemoveV2Mixin(object):
-    def _pre_upgrade_3d581a53980d(self, engine):
+    def _create_topology(self, engine, network_type)
         networks = db_utils.get_table(engine, 'networks')
         ports = db_utils.get_table(engine, 'ports')
         portbindingports = db_utils.get_table(engine, 'portbindingports')
@@ -28,30 +28,60 @@ class RemoveV2Mixin(object):
             'midonet_port_bindings')
         midonet_network_bindings = db_utils.get_table(engine,
             'midonet_network_bindings')
-        data = {
-        }
 
         network_id = uuidutils.generate_uuid()
         network_dict = dict(id=network_id)
         engine.execute(networks.insert().values(network_dict))
         engine.execute(midonet_network_bindings.insert().values(
-            dict(network_id=network_id, network_type="uplink"),
+            dict(network_id=network_id, network_type=network_type),
         ))
 
         port_id = uuidutils.generate_uuid()
         port_dict = dict(id=port_id, network_id=network_id)
         engine.execute(ports.insert().values(port_dict))
+        host = "host-" + network_type
         engine.execute(portbindingports.insert().values(
-            dict(port_id=port_id, host="ubu7"),
+            dict(port_id=port_id, host=host),
         ))
-        engine.execute(midonet_port_bindings.insert().values(
-            dict(port_id=port_id, interface_name="mn-uplink-virt"),
-        ))
+        if network_type == "uplink":
+            interface_name = "interface-uplink"
+            engine.execute(midonet_port_bindings.insert().values(
+                dict(port_id=port_id, interface_name=interface_name),
+            ))
+        else:
+            interface_name = None
+        return dict(network_id=network_id, port_id=port_id,
+                    host=host, interface_name=inteface_name)
 
+    def _pre_upgrade_3d581a53980d(self, engine):
+        data = {
+            "uplink": self._create_topology(engine, network_type="uplink"),
+            "midonet": self._create_topology(engine, network_type="midonet"),
+        }
         return data
 
     def _check_3d581a53980d(self, engine, data):
-        pass
+        networks = db_utils.get_table(engine, 'networks')
+        ports = db_utils.get_table(engine, 'ports')
+        portbindingports = db_utils.get_table(engine, 'portbindingports')
+        midonet_port_bindings = db_utils.get_table(engine,
+            'midonet_port_bindings')
+        midonet_network_bindings = db_utils.get_table(engine,
+            'midonet_network_bindings')
+        ml2_port_bindings = db_utils.get_table(engine, 'ml2_port_bindings')
+        ml2_port_binding_levels = db_utils.get_table(engine,
+            'ml2_port_binding_levels')
+        ml2_distributed_port_bindings = db_utils.get_table(engine,
+            'ml2_distributed_port_bindings')
+        networksegments = db_utils.get_table(engine, 'networksegments')
+
+        for table in (
+            midonet_port_bindings,
+            midonet_network_bindings,
+            portbindingports,
+        ):
+            rows = engine.execute(table.select()).fetchall()
+            self.assertEquals([], rows)
 
 
 class TestRemoveV2Mysql(RemoveV2Mixin,
