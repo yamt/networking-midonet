01eea2f5f738c0213890ee8fbf34820e24451b30
YAMAMOTO Takashi <yamamoto@midokura.com>
2017-05-16 14:34:46 +0900

    x
    Change-Id: I6ea16932b582f644cc74cdc0489fddac2c4f0c43

--- midonet/neutron/db/v2_migration.py
+++ midonet/neutron/db/v2_migration.py
@@ -71,7 +71,9 @@ def log_calls(func):
 
 
 def check_alembic_versions(context):
-    stmt = sql.select([sql.table('alembic_version', 'version_num')])
+    stmt = sql.select(
+        [sql.column('version_num')]).select_from(sql.table('alembic_version'))
+    LOG.info("stmt %s", str(stmt.compile()))
     for version in context.session.execute(stmt).fetchall():
         if version not in _KNOWN_VERSIONS:
             LOG.error('Unknown version %(version)s', {
